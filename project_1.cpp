4fd8ea46d8cb9ba51b1223fb24ce0165b0850a32
sophia <sophia.alencar@sapo.pt>
2023-11-16 12:40:30 +0000

    r

---
+++ project_1.cpp
@@ -0,0 +1,16 @@
+// cortar na horizontal ou vertical
+// sai da linha se já tiver as dimensoes dadas pelo cliente ou se já não for possível ser uma peça
+
+
+
+// OBJETIVO - calcula o valor máximo de peças que pode ser obtido a cortar o mármore consoante as dimensões dadas
+// recebe X*Y
+// lista com n tipo de pecas (todos com dimensoes diferentes)
+// cada peca tem dimensoes a*b e um preço p
+
+struct piece {
+    int height;
+    int lenght;
+    int price;
+} piece;
+
