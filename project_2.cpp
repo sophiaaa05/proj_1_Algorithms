efb82e2878727212491d53416fa0e02298e0d037
sophia <sophia.alencar@sapo.pt>
2023-12-18 08:53:21 +0000

    hello

---
+++ project_2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n_pessoas , m_relacoes;
+    
+    scanf("%d %d", n_pessoas, m_relacoes);
+
+    for (int i = 0; i <m_relacoes; i++){
+        
+    }
+
+}
